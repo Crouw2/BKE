f527eff9c9441040f1c891e2d61b5f3d514432d5
Mike Folkersma <crouw2@hotmail.com>
2019-04-15 12:36:38 +0200

    supp nigga

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 class Board extends Component {
-	render() {
-		return (
-			<div className="boardGame">
-				<h1>BOARD</h1>
-			</div>
-		);
+	constructor(props) {
+		super(props);
+		this.state = {
+			squares: Array(9).fill(null)
+		};
+	}
+
+	renderSquare(i) {
+		console.log(this.sate);
+		return <Square value={i} />;
 	}
 }
 
